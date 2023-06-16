7c99d96d0387d1b0427fa2e2c2de0748fceab78b
Khurmatillo <khurmatillomamasoliev@gmail.com>
2023-06-16 18:14:30 +0500

    changes

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,41 @@
 import { MouseEventHandler } from "react";
 
+export interface CarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
+export interface FilterProps {
+  manufacturer?: string;
+  year?: number;
+  model?: string;
+  limit?: number;
+  fuel?: string;
+}
+
+export interface HomeProps {
+  searchParams: FilterProps;
+}
+
+export interface CarCardProps {
+  model: string;
+  make: string;
+  mpg: number;
+  transmission: string;
+  year: number;
+  drive: string;
+  cityMPG: number;
+}
 export interface CustomButtonProps {
   title: string;
   containerStyles?: string;
@@ -15,6 +51,11 @@ export interface OptionProps {
 export interface CustomFilterProps {
   title: string;
   options: OptionProps[];
+}
+
+export interface ShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
 }
 
 export interface SearchManuFacturerProps {
